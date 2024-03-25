74290a00834f90b7bc09ebf1829552462ae199f9
otfeb <87967166+otfeb@users.noreply.github.com>
2024-03-25 17:34:55 +0900

    [Gold V] Title: 탑, Time: 888 ms, Memory: 138176 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+stack = []
+result = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > arr[i]:
+            result.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        result.append(0)
+    stack.append([i, arr[i]])
+
+print(*result)
