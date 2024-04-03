4b17f68040a8ff1251a00f16e7e10dca61c6b4e4
otfeb <87967166+otfeb@users.noreply.github.com>
2024-04-03 11:52:30 +0900

    [Gold V] Title: 동전 1, Time: 196 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+dp = [0] * (k+1)
+dp[0] = 1
+coins = []
+
+for i in range(n):
+    coins.append(int(input()))
+
+for coin in coins:
+    for i in range(coin, k+1):
+        dp[i] += dp[i - coin]
+
+print(dp[k])
