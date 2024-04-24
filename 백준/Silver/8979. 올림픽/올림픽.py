c75e69064907213d324e56baaec66af597b0f4a9
otfeb <87967166+otfeb@users.noreply.github.com>
2024-04-24 13:50:39 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31120 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+arr = [[0 for _ in range(5)] for _ in range(n)]
+
+for i in range(n):
+    temp = list(map(int, input().split()))
+    for j in range(4):
+        arr[i][j] = temp[j]
+
+arr.sort(key=lambda x : (x[1], x[2], x[3]))
+arr.reverse()
+
+arr[0][4] = 1
+
+for i in range(1, n):
+    if arr[i][1:4] != arr[i-1][1:4]:
+        arr[i][4] = arr[i-1][4] + 1
+    else:
+        arr[i][4] = arr[i-1][4]
+
+print(arr[k-1][4])
