ae140a9dcb561b8ed01fb78bba45a38355dffb54
otfeb <87967166+otfeb@users.noreply.github.com>
2024-09-04 23:20:52 +0900

    [level 3] Title: 네트워크, Time: 2.67 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,22 @@
+from collections import deque
+
 def solution(n, computers):
     
-    def dfs(x):
-        # 방문 처리
-        visited[x] = 1
-        for i in range(n):
-            if computers[x][i] and not visited[i]:
-                dfs(i)
+    def bfs(x):
+        queue = deque()
+        queue.append(x)
+        while queue:
+            a = queue.popleft()
+            visited[a] = 1
+            for i in range(n):
+                if computers[a][i] and not visited[i]:
+                    queue.append(i)
     
     answer = 0
-    # 방문 배열
     visited = [0 for _ in range(n)]
     for i in range(n):
         if not visited[i]:
-            dfs(i)
+            bfs(i)
             answer += 1
     
     return answer
